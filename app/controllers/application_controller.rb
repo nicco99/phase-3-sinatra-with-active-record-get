276db3d784cb2299677fcd6ef12436d271a5fa36
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-12 11:42:46 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
+require "json"
 class ApplicationController < Sinatra::Base
+ # Add this line to set the Content-Type header for all responses
+ set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    # get all the games from the database
+
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
